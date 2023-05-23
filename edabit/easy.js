e1717fb11d00bd5a52ddfad1920468d69657e4b2
MariusDevelops <brazasmarius@gmail.com>
2023-05-23 23:22:21 +0300

    feat: edabit tuck in array challenge

--- edabit/easy.js
+++ edabit/easy.js
@@ -591,5 +591,49 @@ console.groupCollapsed("EASY");
     Test.assertEquals(sortDrinkByPrice(drinks2), sorted2);
   }
   console.groupEnd();
+
+  console.groupCollapsed(
+    "20. Tuck in Array - https://edabit.com/challenge/7ysTEDruHz2prcJQ9"
+  );
+  {
+    function tuckIn(arr1, arr2) {
+      // return [arr1[0], ...arr2, ...arr1.slice(1)]; <--- also works
+      arr1.splice(1, 0, ...arr2);
+      return arr1;
+    }
+
+    Test.assertEquals(
+      tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]),
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    );
+    Test.assertEquals(tuckIn([15, 150], [45, 75, 35]), [15, 45, 75, 35, 150]);
+    Test.assertEquals(
+      tuckIn(["bottom", "topping"], ["tomatosauce", "vegetables", "cheese"]),
+      ["bottom", "tomatosauce", "vegetables", "cheese", "topping"]
+    );
+    Test.assertEquals(
+      tuckIn(
+        [
+          [1, 2],
+          [5, 6],
+        ],
+        [[3, 4]]
+      ),
+      [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]
+    );
+    Test.assertEquals(tuckIn([true, false], [false, true, null, undefined]), [
+      true,
+      false,
+      true,
+      null,
+      undefined,
+      false,
+    ]);
+  }
+  console.groupEnd();
 }
 console.groupEnd();
