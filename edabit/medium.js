1142739ef1714fdecbd923ec1322be91a357254b
MariusDevelops <brazasmarius@gmail.com>
2023-05-25 23:40:21 +0300

    feat: edabit perimeters with a catch

--- edabit/medium.js
+++ edabit/medium.js
@@ -277,5 +277,26 @@ console.groupCollapsed("MEDIUM");
     Test.assertEquals(shiftToRight(-512, 10), -1);
   }
   console.groupEnd();
+
+  console.groupCollapsed(
+    "6. Perimeters with a Catch - https://edabit.com/challenge/WEvqZTFcHeYzFn74c"
+  );
+  {
+    function perimeter(l, num) {
+      return l === "s" ? 4 * num : 6.28 * num;
+    }
+
+    Test.assertEquals(perimeter("s", 1), 4);
+    Test.assertEquals(perimeter("s", 4), 16);
+    Test.assertEquals(perimeter("s", 9), 36);
+    Test.assertEquals(perimeter("s", 13), 52);
+    Test.assertEquals(perimeter("s", 30), 120);
+    Test.assertEquals(perimeter("c", 1), 6.28);
+    Test.assertEquals(perimeter("c", 4), 25.12);
+    Test.assertEquals(perimeter("c", 9), 56.52);
+    Test.assertEquals(perimeter("c", 13), 81.64);
+    Test.assertEquals(perimeter("c", 30), 188.4);
+  }
+  console.groupEnd();
 }
 console.groupEnd();
