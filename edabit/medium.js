0658c7e8ed78be1f5280fd74898bfc29700dd985
MariusDevelops <brazasmarius@gmail.com>
2023-05-28 19:06:33 +0300

    feat: edabit function factory

--- edabit/medium.js
+++ edabit/medium.js
@@ -492,5 +492,42 @@ console.groupCollapsed("MEDIUM");
     );
   }
   console.groupEnd();
+
+  console.groupCollapsed(
+    "13. Function Factory - https://edabit.com/challenge/ENWFBL4jbTgLbSqwS"
+  );
+  {
+    function makePlusFunction(baseNum) {
+      return function addNum(newNum) {
+        console.log(baseNum, newNum);
+        return baseNum + newNum;
+      };
+    }
+
+    const plusTwo = makePlusFunction(2);
+    const plusFive = makePlusFunction(5);
+    const plusSeven = makePlusFunction(plusTwo(plusFive(0)));
+    const plusTen = makePlusFunction(10);
+
+    Test.assertEquals(plusTwo(0), 2);
+    Test.assertEquals(plusTwo(18), 20);
+    Test.assertEquals(plusTwo(-1), 1);
+    Test.assertEquals(plusFive(0), 5);
+    Test.assertEquals(plusFive(12), 17);
+    Test.assertEquals(plusFive(-5), 0);
+    Test.assertEquals(plusSeven(0), 7);
+    Test.assertEquals(plusSeven(41), 48);
+    Test.assertEquals(plusSeven(-117), -110);
+    Test.assertEquals(plusTen(0), 10);
+    Test.assertEquals(plusTen(1), 11);
+    Test.assertEquals(plusTen(-1), 9);
+    Test.assertEquals(plusTwo(plusFive(plusSeven(plusTen(1)))), 25);
+
+    Test.assertEquals(makePlusFunction(8)(8), 16);
+    Test.assertEquals(makePlusFunction(1)(100), 101);
+    Test.assertEquals(makePlusFunction(-100)(0), -100);
+    Test.assertEquals(makePlusFunction(0)(0), 0);
+  }
+  console.groupEnd();
 }
 console.groupEnd();
